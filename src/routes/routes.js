ca3d0f0e8ff77b0b86d94d394fb55252f3724f14
aguskertha <baguskertha@gmail.com>
2022-06-26 22:02:08 +0700

    feat(admin): add api route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const homeRouter = require('./home.route')
 const authRouter = require('../admin/routes/auth.route')
 const adminRouter = require('./../admin/routes/routes')
+const apiRouter = require('./../api/routes/routes')
 
 router.use('/auth', authRouter);
 router.use('/admin', adminRouter);
+router.use('/api', apiRouter);
 router.use('/', homeRouter);
 
 module.exports = router;
